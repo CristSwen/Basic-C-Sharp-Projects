b849ea215e0604eced317bf2a7b2af1d2ff25ac2
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-04-28 17:47:21 -0600

    Added a Branching assignment
    You can enter in a packages dimensions and weight. The program will end if it is over a certain amount. If it is below the certain amount it will give you the shipping cost

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+
+			Console.WriteLine("What is your package's weight?");
+			decimal packageWeight = Convert.ToDecimal(Console.ReadLine());
+
+			if (packageWeight > 50)
+			{
+				Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good Day");
+				Console.ReadLine();
+			}
+			else
+			{
+				Console.WriteLine("What is the package's width?");
+				decimal packageWidth = Convert.ToDecimal(Console.ReadLine());
+				Console.WriteLine("What is the package's height?");
+				decimal packageHeight = Convert.ToDecimal(Console.ReadLine());
+				Console.WriteLine("What is the package's length?");
+				decimal packageLength = Convert.ToDecimal(Console.ReadLine());
+
+				decimal packageDimension = packageWidth + packageHeight + packageLength;
+				if (packageDimension > 50)
+				{
+					Console.WriteLine("Package too big to be shipped via Package Express. Have a good Day");
+					Console.ReadLine();
+				}
+				else
+				{
+					decimal packageTotal = ((packageWidth * packageHeight * packageLength) * packageWeight);
+					decimal packagePrice = packageTotal / 100;
+					Console.WriteLine("Your estimated total for shipping this package is: " + packagePrice);
+					Console.WriteLine("Thank You");
+					Console.ReadLine();
+				}
+			}
+		}
+	}
+}
+			
