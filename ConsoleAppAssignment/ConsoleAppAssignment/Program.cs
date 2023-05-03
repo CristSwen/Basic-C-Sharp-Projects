e52c9e31f7dba1c10fd571b8531c55a41b9c236f
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-03 13:07:59 -0600

    Added a while loop
    Added a while loop that will loop through my favorite number

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -10,50 +10,84 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please guess what month it is. Ex. '03' ");
-            int month = Convert.ToInt32(Console.ReadLine());
-            //The bool is taking the month variable and making 12 true
-            bool isGuessed = month == 12;
+            ////Console.WriteLine("Please guess what month it is. Ex. '03' ");
+            ////int month = Convert.ToInt32(Console.ReadLine());
+            //////The bool is taking the month variable and making 12 true
+            ////bool isGuessed = month == 12;
 
-            //The Do statement will check if any of the cases have been met.
-            do
+            //////The Do statement will check if any of the cases have been met.
+            ////do
+            ////{
+            ////    switch (month)
+            ////    {
+            ////        case 01:
+            ////            Console.WriteLine("It's not January silly");
+            ////            Console.WriteLine("Guess another month");
+            ////            month = Convert.ToInt32(Console.ReadLine());
+            ////            break;
+
+            ////        case 04:
+            ////            Console.WriteLine("It's not April silly");
+            ////            Console.WriteLine("Guess another month");
+            ////            month = Convert.ToInt32(Console.ReadLine());
+            ////            break;
+
+            ////        case 12:
+            ////            Console.WriteLine("It's not December silly");
+            ////            Console.WriteLine("Guess another month");
+            ////            month = Convert.ToInt32(Console.ReadLine());
+            ////            break;
+
+            ////        //The correct month if guessed.
+            ////        case 05:
+            ////            Console.WriteLine("You finally guessed May. Was about time!");
+            ////            isGuessed = true;
+            ////            break;
+
+            ////        //If none of the other months are guessed, default to this
+            ////        default:
+            ////            Console.WriteLine("Guessed the wrong month!");
+            ////            Console.WriteLine("Guess another month!");
+            ////            month = Convert.ToInt32(Console.ReadLine());
+            ////            break;
+            ////    }
+            ////}
+            ////while (!isGuessed);
+
+            ////Console.Read();
+
+            Console.WriteLine("Try to guess my favorite number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool numGuessed = number == 21;
+
+            while (!numGuessed)
             {
-                switch (month)
+                switch (number)
                 {
-                    case 01:
-                        Console.WriteLine("It's not January silly");
-                        Console.WriteLine("Guess another month");
-                        month = Convert.ToInt32(Console.ReadLine());
+                    case 3:
+                        Console.WriteLine("Yikes! Not 3! Try again!");
+                        Console.WriteLine("Guess another one");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
 
-                    case 04:
-                        Console.WriteLine("It's not April silly");
-                        Console.WriteLine("Guess another month");
-                        month = Convert.ToInt32(Console.ReadLine());
+                    case 50:
+                        Console.WriteLine("A little too high! Try again");
+                        Console.WriteLine("Guess another one");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
 
-                    case 12:
-                        Console.WriteLine("It's not December silly");
-                        Console.WriteLine("Guess another month");
-                        month = Convert.ToInt32(Console.ReadLine());
+                    case 21:
+                        Console.WriteLine("You Guessed it! 21 is the best, because of the meme!");
+                        numGuessed = true;
                         break;
 
-                    //The correct month if guessed.
-                    case 05:
-                        Console.WriteLine("You finally guessed May. Was about time!");
-                        isGuessed = true;
-                        break;
-
-                    //If none of the other months are guessed, default to this
                     default:
-                        Console.WriteLine("Guessed the wrong month!");
-                        Console.WriteLine("Guess another month!");
-                        month = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Wrong number! Try again!");
+                        Console.WriteLine("Guess another one!");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
                 }
             }
-            while (!isGuessed);
-
             Console.Read();
         }
     }
