2d0db6e9dd0cc9cfd1f8f45f24b4d35e34ec5e20
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-04 14:21:16 -0600

    Bug fixed the console app
    Was using the wrong type of loop to iterate through the array. I had the right approach from the get go, but I was using the wrong loop

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = { "text", "what" };
+            Console.WriteLine("Enter in some text");
+            string response = Console.ReadLine();
+
+            //iterating over the indices of the array using a forloop
+            for (int i = 0; i < strArray.Length; i++)
+                
+            {
+
+                strArray[i] = strArray[i] + response;
+            }
+
+            foreach (string i in strArray)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
