f22a534d281184b9b9e0f272eb092888cd0cefd3
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-04 15:23:50 -0600

    Updated console assignment
    Updated the console assignment so that it has gone through parts 1-4

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,24 +10,92 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            string[] strArray = { "text", "what" };
-            Console.WriteLine("Enter in some text");
+            // PART ONE OF CONSOLE APP ASSIGNMENT **********
+
+            //Creates an array and asks the user for a random input word
+            //string[] strArray = { "text", "what" };
+            //Console.WriteLine("Enter in some text");
+            //string response = Console.ReadLine();
+
+            ////iterating over the indices of the array using a forloop and adding them to the string. a foreach loop does not work for this loop
+            //for (int i = 0; i < strArray.Length; i++)
+
+            //{
+
+            //    strArray[i] = strArray[i] + response;
+            //}
+
+            //foreach (string i in strArray)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.ReadLine();
+            //}
+
+
+
+            // PART TWO OF CONSOLE APP ASSIGNMENT ************
+
+            //This is the infinite loop
+            //int num = 5;
+            //while (num < 6)
+            //{
+            //    Console.WriteLine("Oh no, it won't stop");
+
+            //}
+            //Console.ReadLine();
+
+            //This is the infinite loop fixed to not be infinite
+
+            //for (int num = 5; num < 8; num++)
+            //{
+            //    Console.WriteLine(num);
+            //    Console.ReadLine();
+            //}
+
+
+            // PART THREE OF THE CONSOLE APP ASSIGNMENT *********
+            // for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            //for (int i = 0; i <= 6; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+
+
+            // PART FOUR OF THE CONSOLE APP ASSIGNMENT ***********
+            List<string> strList = new List<string>();
+            strList.Add("strawberry");
+            strList.Add("onion");
+            strList.Add("blueberry");
+            strList.Add("seaweed");
+
+            Console.WriteLine("User! Search for a Fruit or Veggie! ex.'apple'");
             string response = Console.ReadLine();
 
-            //iterating over the indices of the array using a forloop
-            for (int i = 0; i < strArray.Length; i++)
+            //This for loop chekcs if response is equal to i
+            for (int i = 0; i < strList.Count; i++)
+            {
+                if (strList[i] == response)
+                {
+                    //if the reponse is equal to i, it will print the index and name
+                    Console.WriteLine("{0}: {1}", response, i);
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    //if the respone is not equal to i, the program will end.
+                    Console.WriteLine("Not there");
+                    Console.ReadLine();
                 
-            {
-
-                strArray[i] = strArray[i] + response;
+                }
             }
-
-            foreach (string i in strArray)
-            {
-                Console.WriteLine(i);
-                Console.ReadLine();
-            }
-
         }
     }
 }
