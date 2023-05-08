1e6fc8c3711d7302f95bf20612657bbd730952c8
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-08 17:29:52 -0600

    Created method class assignment
    Created an assignment that would create a void method to take in integers

---
+++ MethodClassAssignment/MethodClassAssignment/Integers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Integers
+    {
+        public void Numbers(int number, int number2)
+        {
+            int operation = number * 2;
+            Console.WriteLine(number2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integers integers = new Integers();
+            integers.Numbers(5, 7);
+            integers.Numbers(number, number2);
+
+           
+        }
+    }
+}
