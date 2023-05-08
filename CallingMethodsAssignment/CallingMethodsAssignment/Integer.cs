62e21664dad2d53c56fd1cc04cd2abe46f0e41db
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-08 15:47:16 -0600

    Added Class assignment
    This assignment makes use of classes for more organized coding and understanding how to instantiate them

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Integer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Integer
+    {
+        //multiplication
+        public int Multiply(int number)
+        {
+            return number * 2;
+        }
+
+        //addition
+        public int Addition(int number)
+        {
+            return number + 4;
+        }
+        //subtraction
+        public int Subtraction(int number)
+        {
+            return number - 7;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This instantiates the Integer class
+            Integer integer = new Integer();
+
+            Console.WriteLine("Please enter a whole number");
+            int responseOne = Convert.ToInt32(Console.ReadLine());
+
+            //for multiply
+            int multResult = integer.Multiply(responseOne);
+            Console.WriteLine(multResult);
+            Console.ReadLine();
+
+            //For Addition
+            int addResult = integer.Addition(responseOne);
+            Console.WriteLine(addResult);
+            Console.ReadLine();
+
+            //For Subtraction
+            int subResult = integer.Subtraction(responseOne);
+            Console.WriteLine(subResult);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
