9c280710075adcbed720f4eb880ca428f9a9a933
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-08 16:54:01 -0600

    Added a Main method assignment
    Created the main Method assignment where it makes use of overloading methods.

---
+++ MainMethodAssignment/MainMethodAssignment/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Numbers
+    {
+        public int myFunc(int num)
+        {
+            return num / 12;
+        }
+
+        public double myFunc(double num)
+        {
+            return num + 3;
+        }
+
+        public int myFunc(string num)
+        {
+            int newnum = Convert.ToInt32(num);
+            return newnum + 7;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            //Console.WriteLine("Please enter a integer");
+            //int intResult = Convert.ToInt32(Console.ReadLine());
+            //int firstResult = numbers.myFunc(intResult);
+            //Console.WriteLine(firstResult);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Please enter a decimal");
+            //double floResult = Convert.ToDouble(Console.ReadLine());
+            //double secondResult = numbers.myFunc(floResult);
+            //Console.WriteLine(secondResult);
+            //Console.ReadLine();
+
+            Console.WriteLine("Please enter a number");
+            string strResult = Console.ReadLine();
+            int lastResult = numbers.myFunc(strResult);
+            Console.WriteLine(lastResult);
+            Console.ReadLine();
+        }
+    }
+}
