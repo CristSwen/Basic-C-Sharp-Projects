c1cb54ea2d2114836c3917e1e3b7a3d54bfcc764
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-09 16:20:47 -0600

    Created new assignment
    Created new assignment for Methods to be able to ask the user for two numbers and the second one can be empty

---
+++ MethodAssignment/MethodAssignment/Integers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Integers
+    {
+        public int IntMethod(int num1, int num2)
+        {
+            int newnum = num1 * 3;
+            return newnum;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integers integers = new Integers();
+            Console.WriteLine("Please enter a number");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number if you wish");
+            string secondEnter = Console.ReadLine();
+            int secondNum = 0;
+            if (!string.IsNullOrEmpty(secondEnter))
+            {
+                secondNum = Convert.ToInt32(secondEnter);
+            }
+            int result = integers.IntMethod(firstNum, secondNum);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
