21ac23daa86eb25e47890f577b14db6337c4f2bc
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-09 13:29:12 -0600

    Created new assignment
    Created a new assignment for learning about classes

---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voided voided = new Voided();
+
+            Console.WriteLine("Please enter a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            voided.IntMethod(answer);
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Voided.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Voided
+    {
+        public void IntMethod(int num)
+        {
+            int operation = num / 2;
+            Console.WriteLine(operation);
+            Console.ReadLine();
+        }
+    }
+}
