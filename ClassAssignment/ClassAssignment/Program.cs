b60a40289de6f45f17d3f62d95a64a2200f557ce
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-09 13:56:20 -0600

    Added two new methods
    Added a method with output parameters and added a overloaded method

--- ClassAssignment/ClassAssignment/Program.cs
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -12,8 +12,24 @@ namespace ClassAssignment
         {
             Voided voided = new Voided();
 
-            Console.WriteLine("Please enter a number");
-            int answer = Convert.ToInt32(Console.ReadLine());
+            //Asking the user for a number and printing the response to the screen from the void method
+            //Console.WriteLine("Please enter a number");
+            //int answer = Convert.ToInt32(Console.ReadLine());
+            //voided.IntMethod(answer);
+
+
+            //Printing the out method to the screen
+            //int argNumber;
+            //string argResponse;
+            //voided.OutMethod(out argNumber, out argResponse);
+            //Console.WriteLine(argNumber);
+            //Console.WriteLine(argResponse);
+            //Console.ReadLine();
+
+
+            //Asking the user for a number and printing it to the screen using the overloaded method.
+            Console.WriteLine("Please enter a decimal number to add");
+            decimal answer = Convert.ToDecimal(Console.ReadLine());
             voided.IntMethod(answer);
         }
     }
--- ClassAssignment/ClassAssignment/Voided.cs
+++ ClassAssignment/ClassAssignment/Voided.cs
@@ -8,9 +8,25 @@ namespace ClassAssignment
 {
     class Voided
     {
+        //A method that will return the user inputted number divided by 2
         public void IntMethod(int num)
         {
             int operation = num / 2;
+            Console.WriteLine(operation);
+            Console.ReadLine();
+        }
+
+        //This is a method with out paramters that will return multiple values.
+        public void OutMethod(out int number, out string response)
+        {
+            number = 23;
+            response = "You shall not pass";
+        }
+
+        //Overloaded version of IntMethod
+        public void IntMethod(decimal num)
+        {
+            decimal operation = num + 52;
             Console.WriteLine(operation);
             Console.ReadLine();
         }
