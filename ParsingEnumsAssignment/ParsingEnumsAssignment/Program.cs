fc57a8b67c49994083b5ce46ee5eeeb8989ed4c2
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-10 17:10:55 -0600

    Fixed the assignment
    Put everything in the try catch block. Removed the For loops, and encapsulated everything in the if else statemetns

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("User! Enter the current day of the week, ex. Monday");
+                string answer = Console.ReadLine();
+                DaysOfTheWeek day;
+                if (Enum.TryParse(answer, out day))
+                {
+                    Console.WriteLine(day);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
