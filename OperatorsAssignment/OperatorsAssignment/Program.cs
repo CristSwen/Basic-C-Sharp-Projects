e31110e8ccae2e7cb3d1e5b7fa47fa884cf4d0ab
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-10 15:19:17 -0600

    Added comments
    Added some comments throughout the code

--- OperatorsAssignment/OperatorsAssignment/Program.cs
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -11,9 +11,11 @@ namespace OperatorsAssignment
         static void Main(string[] args)
         {
             Employee employee = new Employee();
+            //Setting the Ids to numbers to compare
             Employee employee1 = new Employee() { Id = 1 };
             Employee employee2 = new Employee() { Id = 2 };
 
+            //Printing the bool comparisons to the console
             Console.WriteLine(employee1 == employee2);
             Console.WriteLine(employee1 != employee2);
             Console.ReadLine();
