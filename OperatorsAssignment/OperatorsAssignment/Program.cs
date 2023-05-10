b672837c5bc04afe908bee4b8206ff195064984e
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-10 14:50:26 -0600

    Fixed the main program
    Couldn't figure out how to print the results. created bools to input the id1 and id2 and printed those

--- OperatorsAssignment/OperatorsAssignment/Program.cs
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -11,6 +11,15 @@ namespace OperatorsAssignment
         static void Main(string[] args)
         {
             Employee employee = new Employee();
+            int id1 = employee.Id = 12;
+            int id2 = employee.Id = 12;
+
+            bool equal = id1 == id2;
+            bool notEqual = id1 != id2;
+
+            Console.WriteLine(equal);
+            Console.WriteLine(notEqual);
+            Console.ReadLine();
         }
     }
 }
