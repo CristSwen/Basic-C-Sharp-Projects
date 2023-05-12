b7de50de2975579af8cd3bb8268ea12e61e20c2a
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-11 20:24:45 -0600

    Created new datetime assignment
    If you input a number it will add that hour to the current time

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("Yo. Type a number");
+            int answer = int.Parse(Console.ReadLine());
+
+            DateTime theTime = DateTime.Now.AddHours(answer);
+            Console.WriteLine("Did you know in {0} hours, it will be {1}? Yeah neither did I.", answer, theTime);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
