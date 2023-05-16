4dc07de7b01837f9c82a7ab9582b879e8b2da0e6
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-16 17:17:08 -0600

    Added a words class
    Added a words class that chains two constructors

---
+++ ConstructorAssignment/ConstructorAssignment/Words.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Words
+    {
+        public string fName;
+        public string lName;
+
+        public Words(string fName) : this(fName, "Bob")
+        {
+
+        }
+
+        public Words(string fName, string lName)
+        {
+            this.fName = fName;
+            this.lName = lName;
+        }
+    }
+}
