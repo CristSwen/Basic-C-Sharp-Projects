8fabe64280ca39f90c24c9ecf978cf79388e0165
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-16 16:12:08 -0600

    Created new assignment
    Created a new assignment making use of constants and variables.

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Wowza";
+            var varName = "variable";
+
+            string together = name + varName;
+            Console.WriteLine(together);
+            Console.ReadLine();
+        }
+    }
+}
