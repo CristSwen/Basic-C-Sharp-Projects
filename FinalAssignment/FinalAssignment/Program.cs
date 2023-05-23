f2f606c658494e8f8789cbfb6ea3bbc34fb26dfa
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-05-23 16:03:36 -0600

    Created assignment
    Created a Entity framework assignment that will make user of students

---
+++ FinalAssignment/FinalAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first Name");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            string lName = Console.ReadLine();
+            Console.WriteLine("Enter your Grade");
+            string grade = Console.ReadLine();
+            var studentFName = new Students { FName = fName };
+            db.Studentss.Add(studentFName);
+            var studentLName = new Students { LName = lName };
+            db.Studentss.Add(studentLName);
+            var studentGrade = new Students { Grade = grade };
+            db.Studentss.Add(studentGrade);
+            db.SaveChanges();
+
+            //Displaying all the entries in the database
+            var query = from b in db.Students
+                        orderby b.FName
+                        select b;
+
+            Console.WriteLine("All students in the database:");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.FName);
+                Console.WriteLine(item.LName);
+                Console.WriteLine(item.Grade);
+            }
+            Console.ReadLine();
+
+        }
+        public class Students
+        {
+            public string FName { get; set; }
+            public string LName { get; set; }
+            public string Grade { get; set; }
+        }
+        public class StudentInfo
+        {
+            public DbSet<Students> Studentss { get; set; }
+        }
+    }
+}
